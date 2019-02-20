dc509e19243d3820f30e19c6b6b05d560eff8393
cowglow <cowglow@gmail.com>
2019-02-20 19:23:34 +0100

    Viewport restricted

--- index.php
+++ index.php
@@ -39,7 +39,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     <head>
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="viewport" content="initial-scale=1.0, maximum-scale=1.0, user-scalable=0, shrink-to-fit=0">
         <title>My Calculator</title>
         <link rel="stylesheet" href="assets/styles.css">
         <script type="text/javascript" src="assets/main.js"></script>
